e3e170c608e0633c08ad17ac6934ea5e11387e56
Alex <kkledvcent@126.com>
2019-01-01 14:04:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zhoutianjie/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.zhoutianjie.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG","onCLick");
+            }
+        });
+        hookOnClickListener(button);
+    }
+
+    private void hookOnClickListener(View view){
+        try {
+            Method getListenerInfo = View.class.getDeclaredMethod("getListenerInfo");
+            getListenerInfo.setAccessible(true);
+            Object listenerInfo = getListenerInfo.invoke(view);
+            Class<?> listenerInfoClz = Class.forName("android.view.View$ListenerInfo");
+            Field mOnClickListener = listenerInfoClz.getDeclaredField("mOnClickListener");
+            mOnClickListener.setAccessible(true);
+            View.OnClickListener originOnClickListener = (View.OnClickListener) mOnClickListener.get(listenerInfo);
+            View.OnClickListener hookedOnClickListener = new HookedOnClickListener(originOnClickListener);
+            mOnClickListener.set(listenerInfo, hookedOnClickListener);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class HookedOnClickListener implements View.OnClickListener {
+        private View.OnClickListener origin;
+
+        HookedOnClickListener(View.OnClickListener origin) {
+            this.origin = origin;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "hook click", Toast.LENGTH_SHORT).show();
+            Log.i("TAG","Before click, do what you want to to.");
+            if (origin != null) {
+                origin.onClick(v);
+            }
+            Log.i("TAG","After click, do what you want to to.");
+        }
+    }
+
+}
