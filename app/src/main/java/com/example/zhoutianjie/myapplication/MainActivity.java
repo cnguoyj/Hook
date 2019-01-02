174da54ddef0fad5916896ce05a093d1cf0cccd5
zhoutianjie <kkledvcent@126.com>
2019-01-02 16:49:00 +0800

    练习

--- app/src/main/java/com/example/zhoutianjie/myapplication/MainActivity.java
+++ app/src/main/java/com/example/zhoutianjie/myapplication/MainActivity.java
@@ -24,41 +24,70 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("TAG","onCLick");
             }
         });
-        hookOnClickListener(button);
+        // hookOnClickListener(button);
+        hookBtn(button);
     }
 
-    private void hookOnClickListener(View view){
-        try {
-            Method getListenerInfo = View.class.getDeclaredMethod("getListenerInfo");
-            getListenerInfo.setAccessible(true);
-            Object listenerInfo = getListenerInfo.invoke(view);
-            Class<?> listenerInfoClz = Class.forName("android.view.View$ListenerInfo");
-            Field mOnClickListener = listenerInfoClz.getDeclaredField("mOnClickListener");
-            mOnClickListener.setAccessible(true);
-            View.OnClickListener originOnClickListener = (View.OnClickListener) mOnClickListener.get(listenerInfo);
-            View.OnClickListener hookedOnClickListener = new HookedOnClickListener(originOnClickListener);
-            mOnClickListener.set(listenerInfo, hookedOnClickListener);
+//    private void hookOnClickListener(View view){
+//        try {
+//            Method getListenerInfo = View.class.getDeclaredMethod("getListenerInfo");
+//            getListenerInfo.setAccessible(true);
+//            Object listenerInfo = getListenerInfo.invoke(view);
+//            Class<?> listenerInfoClz = Class.forName("android.view.View$ListenerInfo");
+//            Field mOnClickListener = listenerInfoClz.getDeclaredField("mOnClickListener");
+//            mOnClickListener.setAccessible(true);
+//            View.OnClickListener originOnClickListener = (View.OnClickListener) mOnClickListener.get(listenerInfo);
+//            View.OnClickListener hookedOnClickListener = new HookedOnClickListener(originOnClickListener);
+//            mOnClickListener.set(listenerInfo, hookedOnClickListener);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    class HookedOnClickListener implements View.OnClickListener {
+//        private View.OnClickListener origin;
+//
+//        HookedOnClickListener(View.OnClickListener origin) {
+//            this.origin = origin;
+//        }
+//
+//        @Override
+//        public void onClick(View v) {
+//            Toast.makeText(MainActivity.this, "hook click", Toast.LENGTH_SHORT).show();
+//            Log.i("TAG","Before click, do what you want to to.");
+//            if (origin != null) {
+//                origin.onClick(v);
+//            }
+//            Log.i("TAG","After click, do what you want to to.");
+//        }
+//    }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void hookBtn(View view){
+        Class clz = View.class;
+        try{
+            Method method = clz.getDeclaredMethod("getListenerInfo");
+            method.setAccessible(true);
+            //得到listenerInfo
+            Object listenerInfo = method.invoke(view);
+
+            Class listenerInfoClz = Class.forName("android.view.View$ListenerInfo");
+            Field onClickListenerField = listenerInfoClz.getDeclaredField("mOnClickListener");
+
+            HookonClickListener listener = new HookonClickListener();
+            onClickListenerField.set(listenerInfo,listener);
+
+        }catch (Exception e){
+
         }
+
     }
 
-    class HookedOnClickListener implements View.OnClickListener {
-        private View.OnClickListener origin;
-
-        HookedOnClickListener(View.OnClickListener origin) {
-            this.origin = origin;
-        }
+    class HookonClickListener implements View.OnClickListener{
 
         @Override
         public void onClick(View v) {
-            Toast.makeText(MainActivity.this, "hook click", Toast.LENGTH_SHORT).show();
-            Log.i("TAG","Before click, do what you want to to.");
-            if (origin != null) {
-                origin.onClick(v);
-            }
-            Log.i("TAG","After click, do what you want to to.");
+            Log.i("TAG","onCLicKKk");
         }
     }
 
